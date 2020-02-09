4584d4eaffb7a4737dc8bbc27bb4f28be4ffd1b3
Adriano Alonso <adrianojcalonso@gmail.com>
2020-02-09 20:23:45 -0300

    Initial commit

---
+++ src/android/PrintTest.java
@@ -0,0 +1,37 @@
+package cordova-plugin-printtest;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class PrintTest extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("add")) {
+            this.add(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void add(JSONArray args, CallbackContext callback) {
+        if (args != null) {
+            try {
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.sucess(""+(p1+p1));
+            } catch (Exception e) {
+                callback.error("Something went wrong :" + e);
+            }
+        } else {
+            callback.error("Expected one non-empty JSON argument.");
+        }
+    }
+}
