bf5bd7bd1443130d7b3c05556bd7d978ca289e96
Adriano Alonso <adrianojcalonso@gmail.com>
2020-02-13 11:50:28 -0300

    e

--- src/android/PrintTest.java
+++ src/android/PrintTest.java
@@ -15,7 +15,9 @@ public class PrintTest extends CordovaPlugin {
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("add")) {
-            this.add(args, callbackContext);
+            String message = args.getString(0);
+            this.add(message, callbackContext);
+            //this.add(args, callbackContext);
             return true;
         } else if (action.equals("nativeToast")) {
             nativeToast();
@@ -23,18 +25,18 @@ public class PrintTest extends CordovaPlugin {
         return false;
     }
 
-    private void add(JSONArray args, CallbackContext callback) {
-        try{
-        //JSONArray jsonArgs = args.getJSONObject(0);
-        int species = args.getJSONObject(0).getInt("param1");
+    //private void add(JSONArray args, CallbackContext callback) {
+    private void add(String args, CallbackContext callback) {
+        //int species = args.getJSONObject(0).getInt("param1");
         //String sp2 = args.getJSONObject(0).getString("param2");
-        Toast.makeText(webView.getContext(), species, Toast.LENGTH_SHORT).show();
-        }
+        Toast.makeText(webView.getContext(), message, Toast.LENGTH_SHORT).show();
         if (args != null) {
             try {
                 //Toast.makeText(webView.getContext(), "params :"+args.getJSONObject(0).getString("param1")+" e "+args.getJSONObject(0).getString("param2"), Toast.LENGTH_SHORT).show();
-                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                //int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                //int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                int p1 = 1;
+                int p2 = 2;
                 callback.success(""+(p1+p2));
             } catch (Exception e) {
                 callback.error("Something went wrong :" + e);
