112d0c39b0da6d5f558757ee059f12bc67681782
Adriano Alonso <adrianojcalonso@gmail.com>
2020-02-14 14:38:38 -0300

    jason

--- src/android/PrintTest.java
+++ src/android/PrintTest.java
@@ -13,36 +13,30 @@ import android.widget.Toast;
 public class PrintTest extends CordovaPlugin {
 
     @Override
-//    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
-    public boolean execute(String action, String args, CallbackContext callbackContext) throws JSONException {
-            if (action.equals("add")) {
-           
-            //String message = args.getString("param1");
-            Toast.makeText(webView.getContext(), args, Toast.LENGTH_SHORT).show();
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+    //public boolean execute(String action, String args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("add")) {
             this.add(args, callbackContext);
-            //this.add(args, callbackContext);
             return true;
         } else if (action.equals("nativeToast")) {
-            nativeToast(args);
+            String message = args.getJSONObject(0).getString("Message");
+            nativeToast(message);
             return true;
         }
         return false;
     }
 
-    //private void add(JSONArray args, CallbackContext callback) {
-    private void add(String args, CallbackContext callback) {
+    private void add(JSONArray args, CallbackContext callback) {
         //int species = args.getJSONObject(0).getInt("param1");
         //String sp2 = args.getJSONObject(0).getString("param2");
         //Toast.makeText(webView.getContext(), args, Toast.LENGTH_SHORT).show();
         if (args != null) {
             try {
                 //Toast.makeText(webView.getContext(), "params :"+args.getJSONObject(0).getString("param1")+" e "+args.getJSONObject(0).getString("param2"), Toast.LENGTH_SHORT).show();
-                //int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
-                //int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
-                int p1 = 1;
-                int p2 = 2;
-                //callback.success(""+(p1+p2));
-                callback.success("3");
+                int p1 = Integer.parseInt(args.getJSONObject(0).getString("param1"));
+                int p2 = Integer.parseInt(args.getJSONObject(0).getString("param2"));
+                callback.success(""+(p1+p2));
+                //callback.success("3");
             } catch (Exception e) {
                 callback.error("Something went wrong :" + e);
             }
