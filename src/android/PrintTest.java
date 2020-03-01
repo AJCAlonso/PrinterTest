b3b191a92ee2f0b1df94efe69dd2b12474bc7e33
Adriano Alonso <adrianojcalonso@gmail.com>
2020-03-01 19:28:28 -0300

    e

--- src/android/PrintTest.java
+++ src/android/PrintTest.java
@@ -31,30 +31,8 @@ import br.com.gertec.gedi.structs.GEDI_PRNTR_st_StringConfig;
  * This class echoes a string called from JavaScript.
  */
 public class PrintTest extends CordovaPlugin {
-    
-    // Definições
-    private final String IMPRESSORA_ERRO = "Impressora com erro.";
 
-    // Statics
-    private static boolean isPrintInit = false;
-
-    // Vaviáveis iniciais
-    //private Activity activity;
-    private Context context;
-
-    // Classe de impressão
-    private IGEDI iGedi = null;
-    private IPRNTR iPrint = null;
-    private GEDI_PRNTR_st_StringConfig stringConfig;
-    private GEDI_PRNTR_st_PictureConfig pictureConfig;
-    private GEDI_PRNTR_e_Status status;
-
-    // Classe de configuração da impressão
-    //private ConfigPrint configPrint = new ConfigPrint();
-    private ConfigPrint configPrint;
-    private Typeface typeface;
-
-    //private GertecPrinter gertecPrinter;
+    private GertecPrinter gertecPrinter;
 
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
@@ -93,71 +71,5 @@ public class PrintTest extends CordovaPlugin {
     public void nativeToast(String sMessage){
         Toast.makeText(webView.getContext(), sMessage, Toast.LENGTH_SHORT).show();
     }
-
-    // public void GertecPrinter(Activity a, Context c) {
-    //     this.activity = a;
-    //     this.context = c;
-    //     startIGEDI();
-    // }
-
-    // private void startIGEDI() {
-    //     new Thread(() -> {
-    //         this.iGedi = GEDI.getInstance(activity);
-    //         this.iPrint = this.iGedi.getPRNTR();
-    //         try {
-    //             this.ImpressoraInit();
-    //         } catch (GediException e) {
-    //             e.printStackTrace();
-    //         }
-    //     }).start();
-    // }
-
-    // public void setConfigImpressao(ConfigPrint config){
-
-    //     this.configPrint = config;
-
-    //     this.stringConfig = new GEDI_PRNTR_st_StringConfig(new Paint());
-    //     this.stringConfig.paint.setTextSize(configPrint.getTamanho());
-    //     this.stringConfig.paint.setTextAlign(Paint.Align.valueOf(configPrint.getAlinhamento()));
-    //     this.stringConfig.offset = configPrint.getOffSet();
-    //     this.stringConfig.lineSpace = configPrint.getLineSpace();
-
-    //     switch (configPrint.getFonte()){
-    //         case "NORMAL":
-    //             this.typeface = Typeface.create(configPrint.getFonte(), Typeface.NORMAL );
-    //             break;
-    //         case "DEFAULT":
-    //             this.typeface = Typeface.create(Typeface.DEFAULT, Typeface.NORMAL );
-    //             break;
-    //         case "DEFAULT BOLD":
-    //             this.typeface = Typeface.create(Typeface.DEFAULT_BOLD, Typeface.NORMAL );
-    //             break;
-    //         case "MONOSPACE":
-    //             this.typeface = Typeface.create(Typeface.MONOSPACE, Typeface.NORMAL );
-    //             break;
-    //         case "SANS SERIF":
-    //             this.typeface = Typeface.create(Typeface.SANS_SERIF, Typeface.NORMAL );
-    //             break;
-    //         case "SERIF":
-    //             this.typeface = Typeface.create(Typeface.SERIF, Typeface.NORMAL );
-    //             break;
-    //         default:
-    //             this.typeface = Typeface.createFromAsset(this.context.getAssets(), configPrint.getFonte());
-    //     }
-
-    //     if (this.configPrint.isNegrito() && this.configPrint.isItalico()){
-    //         typeface = Typeface.create(typeface, Typeface.BOLD_ITALIC);
-    //     }else if(this.configPrint.isNegrito()){
-    //         typeface = Typeface.create(typeface, Typeface.BOLD);
-    //     }else if(this.configPrint.isItalico()){
-    //         typeface = Typeface.create(typeface, Typeface.ITALIC);
-    //     }
-
-    //     if(this.configPrint.isSublinhado()){
-    //         this.stringConfig.paint.setFlags(Paint.UNDERLINE_TEXT_FLAG);
-    //     }
-
-    //     this.stringConfig.paint.setTypeface(this.typeface);
-    // }
 
 }
