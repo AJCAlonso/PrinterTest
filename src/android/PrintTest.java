d0b0a872be596dfa0c956e92ab5139ee9052ce33
Adriano Alonso <adrianojcalonso@gmail.com>
2020-03-04 14:20:54 -0300

    r

--- src/android/PrintTest.java
+++ src/android/PrintTest.java
@@ -2,6 +2,7 @@ package cordova.plugin.printtest;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
+import org.apache.cordova.CordovaInterface;
 import org.apache.cordova.CordovaWebView;
 
 import org.json.JSONArray;
