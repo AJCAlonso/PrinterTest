cecec6b538a829e74d3f1b70c39eacf8e15a77a7
Adriano Alonso <adrianojcalonso@gmail.com>
2020-03-28 15:51:19 -0300

    j

--- src/android/PrintTest.java
+++ src/android/PrintTest.java
@@ -234,7 +234,7 @@ public class PrintTest extends CordovaPlugin {
 
                     gertecPrinter.imprimeTexto(args.getJSONObject(0).getString("endereco")+ " - "+args.getJSONObject(0).getString("bairro")+"/"+args.getJSONObject(0).getString("estado"));
                     gertecPrinter.imprimeTexto("CEP: " + args.getJSONObject(0).getString("cep"));
-                    gertecPrinter.avancaLinha(60);
+                    gertecPrinter.avancaLinha(80);
                     
                     gertecPrinter.ImpressoraOutput();
                 }else{
